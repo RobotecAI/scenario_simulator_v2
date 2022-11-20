b94b5d2283f1be0647404946ca59430a4888b36a
Adam Krawczyk <adam.krawczyk@robotec.ai>
2022-11-21 00:38:18 +0100

    Reduce rotation matrix calls

--- simulation/simple_sensor_simulator/src/sensor_simulation/lidar/lidar_sensor.cpp
+++ simulation/simple_sensor_simulator/src/sensor_simulation/lidar/lidar_sensor.cpp
@@ -52,12 +52,7 @@ auto LidarSensor<sensor_msgs::msg::PointCloud2>::raycast(
     }
   }
   if (ego_pose) {
-    std::vector<double> vertical_angles;
-    for (const auto v : configuration_.vertical_angles()) {
-      vertical_angles.emplace_back(v);
-    }
-    const auto pointcloud = raycaster_.raycast(
-      "base_link", stamp, ego_pose.get(), configuration_.horizontal_resolution(), vertical_angles);
+    const auto pointcloud = raycaster_.raycast("base_link", stamp, ego_pose.get());
     detected_objects_ = raycaster_.getDetectedObject();
     return pointcloud;
   }
