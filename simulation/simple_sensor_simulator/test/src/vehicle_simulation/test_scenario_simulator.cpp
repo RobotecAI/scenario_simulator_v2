51fb643fd1291a454f6a41bd8d202bf318ebc588
robomic <michal.ciasnocha@robotec.ai>
2024-06-14 16:54:38 +0200

    more tests

--- simulation/simple_sensor_simulator/test/src/vehicle_simulation/test_scenario_simulator.cpp
+++ simulation/simple_sensor_simulator/test/src/vehicle_simulation/test_scenario_simulator.cpp
@@ -15,7 +15,9 @@
 #include <gtest/gtest.h>
 
 #include <ament_index_cpp/get_package_share_directory.hpp>
+#include <functional>
 #include <geometry_msgs/msg/pose.hpp>
+#include <simple_sensor_simulator/exception.hpp>
 #include <simple_sensor_simulator/simple_sensor_simulator.hpp>
 #include <simulation_interface/conversions.hpp>
 #include <simulation_interface/zmq_multi_client.hpp>
@@ -35,9 +37,52 @@ auto makeInitializeRequest() -> simulation_api_schema::InitializeRequest
 auto makeUpdateFrameRequest() -> simulation_api_schema::UpdateFrameRequest
 {
   auto request = simulation_api_schema::UpdateFrameRequest();
+  // to be filled up
   return request;
 }
 
+auto makeDespawnEntityRequest(const std::string name = "name")
+  -> simulation_api_schema::DespawnEntityRequest
+{
+  auto request = simulation_api_schema::DespawnEntityRequest();
+  request.set_name(name);
+  return request;
+}
+
+auto makeSpawnVehicleEntityRequest(const std::string name = "name", const bool is_ego = false)
+  -> simulation_api_schema::SpawnVehicleEntityRequest
+{
+  auto request = simulation_api_schema::SpawnVehicleEntityRequest();
+  const auto params = traffic_simulator_msgs::msg::VehicleParameters{};
+  simulation_interface::toProto(params, *request.mutable_parameters());
+  request.mutable_parameters()->set_name(name);
+  request.set_is_ego(is_ego);
+  return request;
+}
+
+auto makeSpawnPedestrianEntityRequest(const std::string name = "name")
+  -> simulation_api_schema::SpawnPedestrianEntityRequest
+{
+  auto request = simulation_api_schema::SpawnPedestrianEntityRequest();
+  const auto params = traffic_simulator_msgs::msg::PedestrianParameters{};
+  simulation_interface::toProto(params, *request.mutable_parameters());
+  request.mutable_parameters()->set_name(name);
+  return request;
+}
+
+auto makeSpawnMiscObjectEntityRequest(const std::string name = "name")
+  -> simulation_api_schema::SpawnMiscObjectEntityRequest
+{
+  auto request = simulation_api_schema::SpawnMiscObjectEntityRequest();
+  const auto params = traffic_simulator_msgs::msg::MiscObjectParameters{};
+  simulation_interface::toProto(params, *request.mutable_parameters());
+  request.mutable_parameters()->set_name(name);
+  return request;
+}
+
+/**
+ * @note Test initialization correctness with a sample request with the default port (5555).
+ */
 TEST(ScenarioSimulator, initialize_defaultPort)
 {
   auto server = std::thread([] {
@@ -56,13 +101,16 @@ TEST(ScenarioSimulator, initialize_defaultPort)
   server.join();
 }
 
+/**
+ * @note Test initialization correctness with a sample request with a custom port.
+ */
 TEST(ScenarioSimulator, initialize_customPort)
 {
   auto server = std::thread([] {
     rclcpp::init(0, nullptr);
     rclcpp::NodeOptions options;
-    rclcpp::Parameter port_param("port", rclcpp::ParameterValue(1234));
-    options.parameter_overrides().push_back(port_param);
+    options.parameter_overrides().push_back(
+      rclcpp::Parameter{"port", rclcpp::ParameterValue(1234)});
     auto component = std::make_shared<simple_sensor_simulator::ScenarioSimulator>(options);
     rclcpp::spin(component);
   });
@@ -76,6 +124,9 @@ TEST(ScenarioSimulator, initialize_customPort)
   server.join();
 }
 
+/**
+ * @note Test updating frame correctness with a sample request.
+*/
 TEST(ScenarioSimulator, updateFrame_correct)
 {
   auto server = std::thread([] {
@@ -95,9 +146,12 @@ TEST(ScenarioSimulator, updateFrame_correct)
   server.join();
 }
 
+/**
+ * @note Test updating frame correctness with a sample request before requesting initialization.
+ */
 TEST(ScenarioSimulator, updateFrame_noInitialize)
 {
-  // uhhh theres missing "initialized_(false)," in the constructor
+  // undefined behaviour !!! there is missing "initialized_(false)," in the ScenarioSimulator constructor
   auto server = std::thread([] {
     rclcpp::init(0, nullptr);
     rclcpp::NodeOptions options;
@@ -109,6 +163,186 @@ TEST(ScenarioSimulator, updateFrame_noInitialize)
     zeromq::MultiClient(simulation_interface::TransportProtocol::TCP, "localhost", 5555U);
 
   EXPECT_FALSE(multi_client.call(makeUpdateFrameRequest()).result().success());
+
+  rclcpp::shutdown();
+  server.join();
+}
+
+/**
+ * @note Test spawning vehicle entity correctness with a request to spawn
+ * Ego vehicle when Ego vehicle is not spawned yet.
+ */
+TEST(ScenarioSimulator, spawnVehicleEntity_firstEgo)
+{
+  auto server = std::thread([] {
+    rclcpp::init(0, nullptr);
+    rclcpp::NodeOptions options;
+    auto component = std::make_shared<simple_sensor_simulator::ScenarioSimulator>(options);
+    rclcpp::spin(component);
+  });
+
+  auto multi_client =
+    zeromq::MultiClient(simulation_interface::TransportProtocol::TCP, "localhost", 5555U);
+
+  EXPECT_TRUE(multi_client.call(makeInitializeRequest()).result().success());
+  EXPECT_TRUE(multi_client.call(makeSpawnVehicleEntityRequest("ego", true)).result().success());
+
+  rclcpp::shutdown();
+  server.join();
+}
+
+/**
+ * @note Test spawning vehicle entity correctness with a request to spawn a NPC vehicle.
+ */
+TEST(ScenarioSimulator, spawnVehicleEntity_npc)
+{
+  auto server = std::thread([] {
+    rclcpp::init(0, nullptr);
+    rclcpp::NodeOptions options;
+    auto component = std::make_shared<simple_sensor_simulator::ScenarioSimulator>(options);
+    rclcpp::spin(component);
+  });
+
+  auto multi_client =
+    zeromq::MultiClient(simulation_interface::TransportProtocol::TCP, "localhost", 5555U);
+
+  EXPECT_TRUE(multi_client.call(makeInitializeRequest()).result().success());
+  EXPECT_TRUE(multi_client.call(makeSpawnVehicleEntityRequest("npc", false)).result().success());
+
+  rclcpp::shutdown();
+  server.join();
+}
+
+/**
+ * @note Test spawning pedestrian entity correctness with a request to spawn a NPC pedestrian.
+ */
+TEST(ScenarioSimulator, spawnPedestrianEntity)
+{
+  auto server = std::thread([] {
+    rclcpp::init(0, nullptr);
+    rclcpp::NodeOptions options;
+    auto component = std::make_shared<simple_sensor_simulator::ScenarioSimulator>(options);
+    rclcpp::spin(component);
+  });
+
+  auto multi_client =
+    zeromq::MultiClient(simulation_interface::TransportProtocol::TCP, "localhost", 5555U);
+
+  EXPECT_TRUE(multi_client.call(makeInitializeRequest()).result().success());
+  EXPECT_TRUE(multi_client.call(makeSpawnPedestrianEntityRequest("bob")).result().success());
+
+  rclcpp::shutdown();
+  server.join();
+}
+
+/**
+ * @note Test spawning misc object entity correctness with a request to spawn a misc object.
+ */
+TEST(ScenarioSimulator, spawnMiscObjectEntity)
+{
+  auto server = std::thread([] {
+    rclcpp::init(0, nullptr);
+    rclcpp::NodeOptions options;
+    auto component = std::make_shared<simple_sensor_simulator::ScenarioSimulator>(options);
+    rclcpp::spin(component);
+  });
+
+  auto multi_client =
+    zeromq::MultiClient(simulation_interface::TransportProtocol::TCP, "localhost", 5555U);
+
+  EXPECT_TRUE(multi_client.call(makeInitializeRequest()).result().success());
+  EXPECT_TRUE(multi_client.call(makeSpawnMiscObjectEntityRequest("blob")).result().success());
+
+  rclcpp::shutdown();
+  server.join();
+}
+
+/**
+ * @note Test despawning an entity with a request to despawn an existing vehicle.
+ */
+TEST(ScenarioSimulator, despawnEntity_vehicle)
+{
+  auto server = std::thread([] {
+    rclcpp::init(0, nullptr);
+    rclcpp::NodeOptions options;
+    auto component = std::make_shared<simple_sensor_simulator::ScenarioSimulator>(options);
+    rclcpp::spin(component);
+  });
+
+  auto multi_client =
+    zeromq::MultiClient(simulation_interface::TransportProtocol::TCP, "localhost", 5555U);
+
+  EXPECT_TRUE(multi_client.call(makeInitializeRequest()).result().success());
+  EXPECT_TRUE(multi_client.call(makeSpawnVehicleEntityRequest("npc", false)).result().success());
+  EXPECT_TRUE(multi_client.call(makeDespawnEntityRequest("npc")).result().success());
+
+  rclcpp::shutdown();
+  server.join();
+}
+
+/**
+ * @note Test despawning an entity with a request to despawn an existing pedestrian.
+ */
+TEST(ScenarioSimulator, despawnEntity_pedestrian)
+{
+  auto server = std::thread([] {
+    rclcpp::init(0, nullptr);
+    rclcpp::NodeOptions options;
+    auto component = std::make_shared<simple_sensor_simulator::ScenarioSimulator>(options);
+    rclcpp::spin(component);
+  });
+
+  auto multi_client =
+    zeromq::MultiClient(simulation_interface::TransportProtocol::TCP, "localhost", 5555U);
+
+  EXPECT_TRUE(multi_client.call(makeInitializeRequest()).result().success());
+  EXPECT_TRUE(multi_client.call(makeSpawnPedestrianEntityRequest("bob")).result().success());
+  EXPECT_TRUE(multi_client.call(makeDespawnEntityRequest("bob")).result().success());
+
+  rclcpp::shutdown();
+  server.join();
+}
+
+/**
+ * @note Test despawning an entity with a request to despawn an existing misc object.
+ */
+TEST(ScenarioSimulator, despawnEntity_miscObject)
+{
+  auto server = std::thread([] {
+    rclcpp::init(0, nullptr);
+    rclcpp::NodeOptions options;
+    auto component = std::make_shared<simple_sensor_simulator::ScenarioSimulator>(options);
+    rclcpp::spin(component);
+  });
+
+  auto multi_client =
+    zeromq::MultiClient(simulation_interface::TransportProtocol::TCP, "localhost", 5555U);
+
+  EXPECT_TRUE(multi_client.call(makeInitializeRequest()).result().success());
+  EXPECT_TRUE(multi_client.call(makeSpawnMiscObjectEntityRequest("blob")).result().success());
+  EXPECT_TRUE(multi_client.call(makeDespawnEntityRequest("blob")).result().success());
+
+  rclcpp::shutdown();
+  server.join();
+}
+
+/**
+ * @note Test despawning an entity with a request to despawn an entity that does not exist.
+ */
+TEST(ScenarioSimulator, despawnEntity_invalidName)
+{
+  auto server = std::thread([] {
+    rclcpp::init(0, nullptr);
+    rclcpp::NodeOptions options;
+    auto component = std::make_shared<simple_sensor_simulator::ScenarioSimulator>(options);
+    rclcpp::spin(component);
+  });
+
+  auto multi_client =
+    zeromq::MultiClient(simulation_interface::TransportProtocol::TCP, "localhost", 5555U);
+
+  EXPECT_TRUE(multi_client.call(makeInitializeRequest()).result().success());
+  EXPECT_FALSE(multi_client.call(makeDespawnEntityRequest("invalid")).result().success());
 
   rclcpp::shutdown();
   server.join();
